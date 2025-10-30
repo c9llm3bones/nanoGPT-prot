0662c23db05aec70f7a2e7fb47039c0668ac9956
Kostya Kostikov <kostickov.costya@yandex.ru>
2025-10-31 01:33:04 +0300

    UPD: prepare.py moved class and type probabilistic addition to train stage

--- data/prots/prepare.py
+++ data/prots/prepare.py
@@ -57,7 +57,7 @@ def norm_type(x):
     if 'light' in s:
         return '<LIGHT>'
     special_tokens.add(f'<{s.upper()}>')
-    return f'<{s.upper()}>' 
+    return f'<{s.upper()}>'
 
 classes = df['class'].apply(norm_class).tolist()
 types = df['type'].apply(norm_type).tolist()
@@ -67,8 +67,10 @@ if DEBUG:
     print(types[:10])
     print()
 
-# get all sequences w.r.t. config 
+# get all sequences w.r.t. config
 all_sequences = []
+all_classes = []
+all_types = []
 i = 0
 rng = np.random.RandomState(seed) # for deterministic sampling
 for row in ds:
@@ -77,18 +79,16 @@ for row in ds:
         if i % 100 == 0:
             print("row: ", row)
     expr = "<EOS>"
-    if rng.rand() < p_class:
-        expr += norm_class(row.get('class'))
-    if rng.rand() < p_type:
-        expr += norm_type(row.get('type'))
-
-    if use_sequence and row.get('sequence'):  
+    if use_sequence and row.get('sequence'):
         expr += row.get('sequence','').replace('\n','').strip()
     elif not use_sequence and row.get('init_seq'):
         expr += row.get('init_seq','').replace('\n','').strip()
+
     if not re.match(r"^<EOS>(?:<[^>]+>)*$", expr): # ensure to have AA
         all_sequences.append(expr)
-    
+        all_classes.append(norm_class(row.get('class')))
+        all_types.append(norm_type(row.get('type')))
+
     if DEBUG:
         if i % 100 == 0:
             print(expr)
@@ -101,7 +101,7 @@ print(f"Prepared {len(all_sequences)} sequences")
 base_chars = list('ACDEFGHIKLMNPQRSTVWY')
 if use_sequence:
     base_chars.append('-')
-special_tokens = ['<EOS>'] + ['<UNK>'] + sorted(list(special_tokens)) # sort for deterministic encoding 
+special_tokens = ['<EOS>'] + ['<UNK>'] + sorted(list(special_tokens)) # sort for deterministic encoding
 if DEBUG:
     print('classes and types: ', special_tokens)
     print()
@@ -109,7 +109,7 @@ vocab = special_tokens + base_chars
 stoi = { ch:i for i,ch in enumerate(vocab) }
 itos = { i:ch for i,ch in enumerate(vocab) }
 
-# new encoding func 
+# new encoding func
 def encode(seq):
     i = 0
     while i < len(seq):
@@ -127,20 +127,37 @@ def encode(seq):
         yield stoi.get(seq[i], stoi['<UNK>'])
         i += 1
 
-all_lists = [list(encode(seq)) for seq in all_sequences] # lists of encoded seqs
-all_ids = np.concatenate(all_lists).astype(np.uint16) # cat all lists
-print("total tokens (all):", len(all_ids))
-if DEBUG:
-    print(all_ids[:1000])
+all_seq_lists = [list(encode(seq)) for seq in all_sequences] # lists of encoded seqs
+all_seq_ids = np.concatenate(all_seq_lists).astype(np.uint16) # cat all seq lists
 
-n = len(all_ids)
+eos_token = stoi['<EOS>']
+# ids of all <EOS> tokens
+eos_ids = np.where(all_seq_ids == eos_token)[0]
+
+# maps for types and classes w.r.t. eos_ids, -1 for unknown
+seq_class_ids = np.array([stoi.get(c, -1) if c else -1 for c in all_classes], dtype=np.uint16)
+seq_type_ids  = np.array([stoi.get(t, -1) if t else -1 for t in all_types ], dtype=np.uint16)
+
+if DEBUG:
+  for (type_, class_)  in zip(seq_type_ids, seq_class_ids):
+    print(itos[type_], " ", itos[class_])
+
+print("total tokens (all):", len(all_seq_ids) + len(seq_class_ids) + len(seq_type_ids))
+if DEBUG:
+    print(all_seq_ids[:1000])
+
+n = len(all_seq_ids)
 n_train = int(n * 0.9)
-train_ids = all_ids[:n_train]
-val_ids = all_ids[n_train:]
+train_ids = all_seq_ids[:n_train]
+val_ids = all_seq_ids[n_train:]
 print(f"train has {len(train_ids)} tokens")
 print(f"val   has {len(val_ids)} tokens")
+
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+seq_class_ids.tofile(os.path.join(os.path.dirname(__file__), 'seq_class_ids.bin'))
+seq_type_ids.tofile(os.path.join(os.path.dirname(__file__), 'seq_type_ids.bin'))
+eos_ids.tofile(os.path.join(os.path.dirname(__file__), 'eos_ids.bin'))
 
 meta = {
     'vocab_size': len(vocab),
